3b88d6ddc3058d1016fc5d4f5eb06c6b011a9cfe
DaneArt <public.centaurus@gmail.com>
2019-10-10 16:32:37 +0300

    Added links to social networks

--- app/src/main/java/com/daneart/sirpompodor/fragments/AboutFragment.kt
+++ app/src/main/java/com/daneart/sirpompodor/fragments/AboutFragment.kt
@@ -1,6 +1,7 @@
 package com.daneart.sirpompodor.fragments
 
 import android.content.Context
+import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
 import androidx.fragment.app.Fragment
@@ -11,7 +12,7 @@ import androidx.appcompat.widget.AppCompatImageView
 import androidx.navigation.Navigation
 
 import com.daneart.sirpompodor.R
-
+import kotlinx.android.synthetic.main.fragment_about.*
 
 
 class AboutFragment : Fragment() {
@@ -24,6 +25,16 @@ class AboutFragment : Fragment() {
         view.findViewById<AppCompatImageView>(R.id.btn_pop_about).setOnClickListener {
             Navigation.findNavController(view).popBackStack()
         }
+
+        btn_twitter.setOnClickListener {
+            val intent = Intent(Intent.ACTION_VIEW, Uri.parse("https://twitter.com/DaneArt1"))
+            startActivity(intent)
+        }
+        btn_github.setOnClickListener {
+            val intent = Intent(Intent.ACTION_VIEW, Uri.parse("https://github.com/DaneArt"))
+            startActivity(intent)
+        }
+
         return view
     }
 
